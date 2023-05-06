106e55a16461ef1d4c17980c5ab4d7bee7096cac
Bojan Bubanja <bojbub@gmail.com>
2023-05-06 19:46:51 +0200

    Handling Form Submission Wiht JS

--- scripts/config.js
+++ scripts/config.js
@@ -7,3 +7,12 @@ function closePlayerConfig () {
     playerConfigOverlayElement.style.display = 'none';
     backdropElement.style.display = 'none';
 }
+
+function savePlayerConfig(event) {
+event.preventDefault();  
+const formData = new FormData(event.target);
+const enteredPlayername = formData.get('playername');
+console.log(enteredPlayername)
+}
+
+ 
