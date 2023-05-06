01e6615e617cfc279550c12fced2d8c75b698944
Bojan Bubanja <bojbub@gmail.com>
2023-05-06 21:59:00 +0200

    Validating User Input With JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,16 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
-const backdropElement = document.getElementById('backdrop');
-const formElement = document.querySelector('form')
+const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorOutputElement = document.getElementById("cofig-errors");
 
-const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
-const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
+const editPlayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPlayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 
-editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
-editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
+editPlayer1BtnElement.addEventListener("click", openPlayerConfig);
+editPlayer2BtnElement.addEventListener("click", openPlayerConfig);
 
-cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
-backdropElement.addEventListener('click', closePlayerConfig);
+cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
+backdropElement.addEventListener("click", closePlayerConfig);
 
-formElement.addEventListener('submit', savePlayerConfig);
+formElement.addEventListener("submit", savePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,18 +1,23 @@
 function openPlayerConfig() {
-    playerConfigOverlayElement.style.display = 'block';
-    backdropElement.style.display = 'block';
+  playerConfigOverlayElement.style.display = "block";
+  backdropElement.style.display = "block";
 }
 
-function closePlayerConfig () {
-    playerConfigOverlayElement.style.display = 'none';
-    backdropElement.style.display = 'none';
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = "none";
+  backdropElement.style.display = "none";
+  formElement.firstElementChild.classList.remove('error');
+  errorOutputElement.textContent = '';
 }
 
 function savePlayerConfig(event) {
-event.preventDefault();  
-const formData = new FormData(event.target);
-const enteredPlayername = formData.get('playername');
-console.log(enteredPlayername)
-}
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayername = formData.get("playername").trim(); //'    Bojan Bubanja    ' =>  "Bojan Bubanja"
 
- 
+  if (!enteredPlayername) { // enteredPlayername === ' '
+    event.target.firstElementChild.classList.add("error");
+    errorOutputElement.textContent = "Please enter a valid name!";
+    return;
+  }
+}
