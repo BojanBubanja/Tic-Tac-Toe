904fb351101c990361ce2d34a533237257f35b95
Bojan Bubanja <bojbub@gmail.com>
2023-05-07 17:03:39 +0200

    Tracking Selected Fields On The Game Board

--- scripts/game.js
+++ scripts/game.js
@@ -1,28 +1,41 @@
 function startNewGame() {
-    if (players[0].name === '' || players[1].name === '') {
-      alert('Please set custom player names for both players!');
-      return;
-    }
-  
-    activePlayerNameElement.textContent = players[activePlayer].name;
-    gameAreaElement.style.display = 'block';
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please set custom player names for both players!");
+    return;
   }
-  
-  function switchPlayer() {
-    if (activePlayer === 0) {
-      activePlayer = 1;
-    } else {
-      activePlayer = 0;
-    }
-    activePlayerNameElement.textContent = players[activePlayer].name;
+
+  activePlayerNameElement.textContent = players[activePlayer].name;
+  gameAreaElement.style.display = "block";
+}
+
+function switchPlayer() {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
   }
-  
-  function selectGameField(event) {
-    if (event.target.tagName !== 'LI') {
-      return;
-    }
-  
-    event.target.textContent = players[activePlayer].symbol;
-    event.target.classList.add('disabled');
-    switchPlayer();
+  activePlayerNameElement.textContent = players[activePlayer].name;
+}
+
+function selectGameField(event) {
+  if (event.target.tagName !== "LI") {
+    return;
   }
+
+  const selectedField = event.target;
+  const selectedColumn = selectedField.dataset.col - 1;
+  const selectedRow = selectedField.dataset.row - 1;
+
+  if (gameData[selectedRow][selectedColumn] > 0) {
+    alert("Please select an empty field!");
+    return;
+  }
+
+  selectedField.textContent = players[activePlayer].symbol;
+  selectedField.classList.add("disabled");
+
+  gameData[selectedRow][selectedColumn] = activePlayer + 1;
+  console.log(gameData);
+
+  switchPlayer();
+}
