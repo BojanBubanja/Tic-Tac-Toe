d24bbcdec2a4f50490ec96bbecedd2e4dd365193
Bojan Bubanja <bojbub@gmail.com>
2023-05-07 15:00:04 +0200

    Managin Game Rounds (Turns) & Field Selection

--- scripts/config.js
+++ scripts/config.js
@@ -1,40 +1,32 @@
 function openPlayerConfig(event) {
-  editedPlayer = +event.target.dataset.playerid; // +'1' => 1
-  playerConfigOverlayElement.style.display = "block";
-  backdropElement.style.display = "block";
-}
-
-function closePlayerConfig() {
-  playerConfigOverlayElement.style.display = "none";
-  backdropElement.style.display = "none";
-  formElement.firstElementChild.classList.remove("error");
-  errorsOutputElement.textContent = "";
-  formElement.firstElementChild.lastElementChild.value = "";
-}
-
-function savePlayerConfig(event) {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const enteredPlayername = formData.get("playername").trim(); // '   Bojan Bubanja    ' => 'Bojan Bubanja'
-
-  if (!enteredPlayername) {
-    // enteredPlayername === ''
-    event.target.firstElementChild.classList.add("error");
-    errorsOutputElement.textContent = "Please enter a valid name!";
-    return;
+    editedPlayer = +event.target.dataset.playerid; // +'1' => 1
+    playerConfigOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
   }
-
-  const updatedPlayerDataElement = document.getElementById(
-    "player-" + editedPlayer + "-data"
-  );
-  updatedPlayerDataElement.children[1].textContent = enteredPlayername;
-
-  // if (editedPlayer === 1) {
-  //     players[0].name = enteredPlayername;
-  //     players[0].name = enteredPlayername;
-  // }
-
-  players[editedPlayer - 1].name = enteredPlayername;
-
-  closePlayerConfig();
-}
+  
+  function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+    formElement.firstElementChild.classList.remove('error');
+    errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';
+  }
+  
+  function savePlayerConfig(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const enteredPlayername = formData.get('playername').trim(); // '  Bojan Bubanja    ' => 'Bojan Bubanja'
+  
+    if (!enteredPlayername) { // enteredPlayername === ''
+      event.target.firstElementChild.classList.add('error');
+      errorsOutputElement.textContent = 'Please enter a valid name!';
+      return;
+    }
+  
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayername;
+  
+    players[editedPlayer - 1].name = enteredPlayername;
+  
+    closePlayerConfig();  
+  }
